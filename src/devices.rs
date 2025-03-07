d47eca1c6c61d6515574dfe10c352dbf50f1c144
Lucas Thelen <thelenlucas028@gmail.com>
2025-03-07 16:22:41 -0600

    Ooops

---
+++ src/devices.rs
@@ -0,0 +1,11 @@
+use bincode::{Decode, Encode};
+use defmt::Format;
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Encode, Decode, Format)]
+pub enum DeviceIdentifier {
+    Jupiter,
+    Icarus,
+    Ejector,
+    Atmega,
+    Relay,
+}
